81dfb3a77e406a9c29e91829a41b5a74ba6715c5
rafeh01 <rafehqazi1@gmail.com>
2015-09-08 06:35:41 -0500

    Fixing invalid module name 2048merge.py to merge2048.py

--- coursera/POC 1/merge2048.py
+++ coursera/POC 1/merge2048.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep  3 17:55:56 2015
-2048_merge_attempt1.py
+merge2048.py
 @author: Rafeh
 """
 import unittest
@@ -41,28 +41,6 @@ def merge(nums):
         pairs.append(prev)
     pairs.extend([0] * (len(nums) - len(pairs)))
     return pairs
-
-
-class Test(unittest.TestCase):
-        # python -m unittest 2048_merge_attempt1.py in Terminal
-    def test1(self):
-        # self.assertEqual(function(input), expected_output)
-        self.assertEqual(merge([0, 0, 2, 2]), [4, 0, 0, 0])
-
-    def test2(self):
-        self.assertEqual(merge([2, 0, 2, 4]), [4, 4, 0, 0])
-
-    def test3(self):
-        self.assertEqual(merge([2, 2, 0, 0]), [4, 0, 0, 0])
-
-    def test4(self):
-        self.assertEqual(merge([2, 2, 2, 2, 2]), [4, 4, 2, 0, 0])
-
-    def test5(self):
-        self.assertEqual(merge([8, 16, 16, 8]), [8, 32, 8, 0])
-
-    def test6(self):
-        self.assertEqual(merge([8, 16, 16, 8, 2, 6]), [8, 32, 8, 2, 6, 0])
 
 if __name__ == '__main__':
     import doctest
