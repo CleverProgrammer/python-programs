4679e96e163dd3c9c4d38350acab77f315e43ca9
rafeh01 <rafehqazi1@gmail.com>
2015-09-12 02:55:13 -0500

    updating fullgame2048.py with unittests

--- coursera/POC 1/fullgame2048.py
+++ coursera/POC 1/fullgame2048.py
@@ -127,23 +127,16 @@ class TwentyFortyEight:
 
 
 #poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
-class Test(unittest.TestCase):
-        # python -m unittest 2048_merge_attempt1.py in Terminal
-    def test_merge1(self):
-        # self.assertEqual(function(input), expected_output)
+class TestFullGame2048(unittest.TestCase):
+
+    def test_merge(self):
         self.assertEqual(merge([0, 0, 2, 2]), [4, 0, 0, 0])
-
-    def test_merge2(self):
         self.assertEqual(merge([2, 0, 2, 4]), [4, 4, 0, 0])
-
-    def test_merge3(self):
         self.assertEqual(merge([2, 2, 0, 0]), [4, 0, 0, 0])
-
-    def test_merge4(self):
         self.assertEqual(merge([2, 2, 2, 2, 2]), [4, 4, 2, 0, 0])
-
-    def test_merge5(self):
         self.assertEqual(merge([8, 16, 16, 8]), [8, 32, 8, 0])
-
-    def test_merge6(self):
         self.assertEqual(merge([8, 16, 16, 8, 2, 6]), [8, 32, 8, 2, 6, 0])
+
+
+if __name__ == '__main__':
+    unittest.main()
