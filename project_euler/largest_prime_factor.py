86c3780cbfaf4dfe182157b6da08686f467f7bcb
rafeh01 <rafehqazi1@gmail.com>
2015-09-20 13:06:45 -0500

    updating euler

---
+++ project_euler/largest_prime_factor.py
@@ -0,0 +1,16 @@
+def prime_checker(number):
+ if number == 2:
+  return True
+ for i in range(2, number//2+1):
+  if number%i == 0:
+   return False
+ return True
+
+def largest_prime_factor(number):
+ for i in range(2, number//2+1)[::-1]:
+  if prime_checker(i):
+   if number % i == 0:
+    return i
+ return False
+
+print(largest_prime_factor(200000))
