b1e0ea935116268482967168e76d94861bd19e70
rafeh01 <rafehqazi1@gmail.com>
2015-09-21 10:55:33 -0500

    largest prime update

--- project_euler/largest_prime_factor.py
+++ project_euler/largest_prime_factor.py
@@ -1,16 +1,25 @@
-def prime_checker(number):
- if number == 2:
+def prime(number):
+ if number in [2, 3, 5, 7]:
   return True
- for i in range(2, number//2+1):
-  if number%i == 0:
-   return False
+ elif number%2 == 0:
+  return False
+ else:
+  for i in range(2, number//4+1, 2):
+   if number%i == 0:
+    return False
  return True
 
 def largest_prime_factor(number):
- for i in range(2, number//2+1)[::-1]:
-  if number % i == 0:
-   if prime_checker(i):
-    return i
+ if prime(number//2):
+  return True
+ else:
+  for i in range(2, number//4+1)[::-1]:
+   if number % i == 0:
+    if prime(i):
+     return i
  return False
 
+print(prime(786))
+print(largest_prime_factor(13195))
+print(largest_prime_factor(200000))
 print(largest_prime_factor(600851475143))
