616fa265e36458aa3efcba63b86646053d374f89
rafeh01 <rafehqazi1@gmail.com>
2015-09-25 03:57:05 -0500

    lol

--- coursera/poc1/week3/tic_tac_toe.py
+++ coursera/poc1/week3/tic_tac_toe.py
@@ -33,26 +33,42 @@ def mc_trial(board, player):
     The modified board will contain the state of the game, so the function does not return anything.
     In other words, the function should modify the board input.
     '''
-    for i in range(x):
-        move = random.randrange(1, 9)
-        pass
+    in_progress = lambda: not board.check_win()
+    while in_progress():
+        row, col = random.choice(board.get_empty_squares())
+        board.move(row, col, player)
+        provided.switch_player(player)
 
 
-def mc_update(scores, board, player):
+def mc_update_scores(scores, board, player):
     '''
-    his function takes a grid of scores (a list of lists) with the same dimensions as the Tic-Tac-Toe board,
+    This function takes a grid of scores (a list of lists) with the same dimensions as the Tic-Tac-Toe board,
     a board from a completed game, and which player the machine player is. The function should score the
     completed board and update the scores grid. As the function updates the scores grid directly,
     it does not return anything.
     '''
+    # Scores probably looks like: [ [0,0,0], [0,0,0], [0,0,0] ]
+    if player == board.check_win():
+        sign = 1
+    elif player != board.check_win():
+        sign = -1
+    elif board.check_win() == provided.DRAW:
+        return
+    for row in scores:
+        for col in row:
+            if board.square(row, col) == player:
+                scores[row][col] += sign * SCORE_CURRENT
+            if board.square(row, col) != provided.EMPTY:
+                scores[row][col] -= sign * SCORE_OTHER
+    print(scores)
 
 
-class Tic_Tac_Toe(unittest.TestCase):
+class TTT(unittest.TestCase):
 
     def test_function_name(self):
         # self.assertEqual(function_name([0, 0, 2, 2]), [4, 0, 0, 0])
         pass
 
 
-suite = unittest.TestLoader().loadTestsFromTestCase(Tic_Tac_Toe)
+suite = unittest.TestLoader().loadTestsFromTestCase(TTT)
 unittest.TextTestRunner(verbosity=2).run(suite)
