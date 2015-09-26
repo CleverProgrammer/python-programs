794e932f8953d7950739f9b28ee470285ae56d22
rafeh01 <rafehqazi1@gmail.com>
2015-09-26 06:46:32 -0500

    bs4's naming tuple galore row.time.string

--- api/craigslist_scraper.py
+++ api/craigslist_scraper.py
@@ -11,10 +11,11 @@ def parse_results(search_term):
     soup = BeautifulSoup(r.text)
     rows = soup.find('div', 'content').find_all('p', 'row')
     for row in rows:
-        price = rows[0].find_all('span')[0].get_text()[1:]
         url = 'http://chicago.craigslist.org' + row.a['href']
-        date = row.find_all('span')[3].get_text()[0:7]
+        price = row.find_all('span')[0].get_text()[1:]
+        date = row.time.string
         title = row.find_all('a')[1].get_text()
-        results.append({'price': int(price), 'url': url, 'date': date, 'title': title})
+        results.append({'price': price, 'url': url, 'date': date, 'title': 
+title})
     return results
 
