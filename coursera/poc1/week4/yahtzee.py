b03368a5f4a70bfef8b463a092a134ef5fa47ba2
rafeh01 <rafehqazi1@gmail.com>
2015-09-28 16:47:00 -0500

    Yahtzee completely working

--- coursera/poc1/week4/yahtzee.py
+++ coursera/poc1/week4/yahtzee.py
@@ -1,10 +1,10 @@
+
 """
 Planner for Yahtzee
 Simplifications:  only allow discard and roll, only score against upper level
 """
-import unittest
-# import codeskulptor
-# codeskulptor.set_timeout(20)
+
+ALL_HANDS = range(1, 7)
 
 
 def gen_all_sequences(outcomes, length):
@@ -29,12 +29,17 @@ def score(hand):
     """
     Compute the maximal score for a Yahtzee hand according to the
     upper section of the Yahtzee score card.
-
     hand: full yahtzee hand
-
-    Returns an integer score 
+    Returns an integer score
+    >>> score((1, 5, 5, 2, 3))
+    10
+    >>> score((1, 4, 1, 4, 6))
+    8
+    >>> score((1, 1, 1, 1, 1))
+    5
+    >>> score((2, 3, 4, 5, 6))
+    6
     """
-    #  hand looks like --> (1,2,3,4,5,6)
     max_value = 0
     for num in hand:
         if num * hand.count(num) > max_value:
@@ -46,74 +51,83 @@ def expected_value(held_dice, num_die_sides, num_free_dice):
     """
     Compute the expected value based on held_dice given that there
     are num_free_dice to be rolled, each with num_die_sides.
-
-    held_dice:     dice that you will hold (tuple)
+    held_dice: dice that you will hold
     num_die_sides: number of sides on each die
     num_free_dice: number of dice to be rolled
-
     Returns a floating point expected value
+    >>>expected_value((2, 2), 6, 2)
+    5.833333333333333
     """
-    #  (1,2,3,4,5,6) / 6  ==> 3.5
-    held_dice = (2, 2)
-    num_die_sides = 6
-    num_free_dice = 5
-    enumerations = gen_all_sequences(num_die_sides, num_free_dice)  #
-    possible_hands = []
-    
-    return 0.0
+    new_score = 0
+    total_score = 0.0
+    all_possibilities_for_free_dice = gen_all_sequences(range(1, num_die_sides + 1), num_free_dice)
+
+    if not all_possibilities_for_free_dice:
+        return score(held_dice)
+
+    for possibility in all_possibilities_for_free_dice:
+        new_hand = possibility + held_dice
+        print (new_hand, "-->", score(new_hand))
+        new_score = score(new_hand)
+        total_score += new_score
+    return total_score / len(all_possibilities_for_free_dice)
 
 
 def gen_all_holds(hand):
     """
     Generate all possible choices of dice from hand to hold.
-
     hand: full yahtzee hand
-
     Returns a set of tuples, where each tuple is dice to hold
+
     """
-    return set([()])
+    #  Use this to cross-reference impossible holds
+    hand_value_counter = {}
+    for value in hand:
+        if value not in hand_value_counter:
+            hand_value_counter[value] = 1
+        else:
+            hand_value_counter[value] += 1
+
+    # Generate all possible holds
+    hold_choices = []
+    for length in range(len(hand) + 1):
+        for held_cards in gen_all_sequences(hand, length):
+            hold_choices.append(held_cards)
+    hold_choices = set([tuple(sorted(x)) for x in hold_choices])
+
+    # Store impossible holds indexes
+    hold_choices_indices_to_remove = []
+    hold_choices = list(hold_choices)
+    for impossible_tuple_idx, tuple_ in enumerate(hold_choices):
+        for value in tuple_:
+            if tuple_.count(value) > hand_value_counter[value]:
+                hold_choices_indices_to_remove.append(impossible_tuple_idx)
+                break
+
+    # Remove impossible indexes from hold_choice
+    for impossible_hold_idx in hold_choices_indices_to_remove[::-1]:
+        hold_choices.pop(impossible_hold_idx)
+    print(len(hold_choices))
+    return set(hold_choices)
 
 
 def strategy(hand, num_die_sides):
     """
     Compute the hold that maximizes the expected value when the
     discarded dice are rolled.
-
     hand: full yahtzee hand
     num_die_sides: number of sides on each die
-
     Returns a tuple where the first element is the expected score and
     the second element is a tuple of the dice to hold
     """
-    return (0.0, ())
+    max_expected = 0.0
+    max_hand = None
+    max_length = len(hand)
+    for hand in gen_all_holds(hand):
+        expected_valuE = expected_value(hand, num_die_sides, max_length - len(hand))
+        if expected_valuE > max_expected:
+            max_expected = expected_valuE
+            max_hand = hand
+            return (max_expected, max_hand)
 
-
-def run_example():
-    """
-    Compute the dice to hold and expected score for an example hand
-    """
-    num_die_sides = 6
-    hand = (1, 1, 1, 5, 6)
-    hand_score, hold = strategy(hand, num_die_sides)
-    print("Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score)
-
-
-# run_example()
-
-class Yahtzee(unittest.TestCase):
-
-    def test_score(self):
-        self.assertEqual(score([6, 4, 4, 2, 2, 2]), 8)
-        self.assertEqual(score([6, 4, 4, 2, 2, 6]), 12)
-        self.assertEqual(score([1, 1, 1, 1, 1, 5]), 5)
-
-    def test_expected_value(self):
-        self.assertAlmostEqual(expected_value((2, 2), 6, 2), 5.8333333333)
-
-
-suite = unittest.TestLoader().loadTestsFromTestCase(Yahtzee)
-unittest.TextTestRunner(verbosity=2).run(suite)
-
-
-#import poc_holds_testsuite
-# poc_holds_testsuite.run_suite(gen_all_holds)
+# Strategy( (2,1), 6)
