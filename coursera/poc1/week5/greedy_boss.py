1a14e413f2e07f4752b0d19a27b391c03ef62347
rafeh01 <rafehqazi1@gmail.com>
2015-10-03 01:39:11 -0500

    updating graphs related code into greedy_boss

--- coursera/poc1/week5/greedy_boss.py
+++ coursera/poc1/week5/greedy_boss.py
@@ -46,16 +46,15 @@ def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type=STANDARD):
             if current_day > days_in_simulation:
                 return days_vs_earnings
             current_money -= current_bribe_cost
-            days_vs_earnings.append((current_day, total_salary))
+            if plot_type == STANDARD:
+                days_vs_earnings.append((current_day, total_salary))
+            else:
+                days_vs_earnings.append([math.log(current_day), math.log(total_salary)])
             current_salary += SALARY_INCREMENT
             current_bribe_cost += bribe_cost_increment
 
         # update list with days vs total salary earned for most recent bribe
         # use plot_type to control whether regular or log/log plot
-        if plot_type == STANDARD:
-            days_vs_earnings.append((current_day, total_salary))
-        else:
-            days_vs_earnings.append([math.log(current_day), math.log(total_salary)])
 
     return days_vs_earnings
 
@@ -73,9 +72,9 @@ def run_simulations():
     simpleplot.plot_lines("Greedy boss", 600, 600, "days", "total earnings",
                           [inc_0, inc_500, inc_1000, inc_2000], False,
                           ["Bribe increment = 0", "Bribe increment = 500",
-                              "Bribe increment = 1000", "Bribe increment = 2000"])
+                           "Bribe increment = 1000", "Bribe increment = 2000"])
 
-run_simulations()
+#run_simulations()
 
 
 class GreedyBoss(unittest.TestCase):
