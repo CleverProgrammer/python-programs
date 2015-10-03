2c99b38cc149fc0491390d23c5b9ddc0fefd3e9b
rafeh01 <rafehqazi1@gmail.com>
2015-10-03 01:20:55 -0500

    completed greedy_boss.py

--- coursera/poc1/week5/greedy_boss.py
+++ coursera/poc1/week5/greedy_boss.py
@@ -1,6 +1,9 @@
 """
-Greedy Boss Simulator
-Simulator for greedy boss scenario
+Author: Rafeh Qazi
+Program: Simulator for greedy boss scenario
+Class: Coursera POC 1 Week 5
+Date: 10/03/2015
+Link: http://www.codeskulptor.org/#user40_hEhvJIG1CB_2.py
 """
 import unittest
 # import simpleplot
@@ -34,19 +37,11 @@ def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type=STANDARD):
 
     # Each iteration of this while loop simulates one bribe
     while current_day <= days_in_simulation:
-
-        print("before current_day: %s" % str(current_day))
-        print("before current_money: %s" % current_money)
         days_till_next_bribe = math.ceil((current_bribe_cost - current_money) / current_salary)
         current_day += days_till_next_bribe
         current_money += current_salary * days_till_next_bribe
         total_salary += current_salary * days_till_next_bribe
-        print("current_day: %s" % str(current_day))
-        print("current_bribe_cost: %s" % current_bribe_cost)
-        print("current_salary: %s" % current_salary)
-        print("current_money: %s" % current_money)
-        print("total_salary: %s" % str(total_salary))
-        print()
+        # check whether we have enough savings to bribe without waiting
         while current_money >= current_bribe_cost:
             if current_day > days_in_simulation:
                 return days_vs_earnings
@@ -54,15 +49,13 @@ def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type=STANDARD):
             days_vs_earnings.append((current_day, total_salary))
             current_salary += SALARY_INCREMENT
             current_bribe_cost += bribe_cost_increment
-        # current_day += days_vs_earnings[-1][0]
-        # check whether we have enough savings to bribe without waiting
-
-        # advance current_day to day of next bribe (DO NOT INCREMENT BY ONE DAY)
-
-        # update state of simulation to reflect bribe
 
         # update list with days vs total salary earned for most recent bribe
         # use plot_type to control whether regular or log/log plot
+        if plot_type == STANDARD:
+            days_vs_earnings.append((current_day, total_salary))
+        else:
+            days_vs_earnings.append([math.log(current_day), math.log(total_salary)])
 
     return days_vs_earnings
 
@@ -82,25 +75,23 @@ def run_simulations():
                           ["Bribe increment = 0", "Bribe increment = 500",
                               "Bribe increment = 1000", "Bribe increment = 2000"])
 
-# run_simulations()
-
-
-print(greedy_boss(35, 100))
-# should print [(0, 0), (10, 1000), (16, 2200), (20, 3400), (23, 4600), (26, 6100), (29, 7900), (31, 9300), (33, 10900), (35, 12700)]
-
-# print(greedy_boss(35, 0))
-# should print [(0, 0), (10, 1000), (15, 2000), (19, 3200), (21, 4000),
-# (23, 5000), (25, 6200), (27, 7600), (28, 8400), (29, 9300), (30, 10300),
-# (31, 11400), (32, 12600), (33, 13900), (34, 15300), (34, 15300), (35,
-# 16900)]
+run_simulations()
 
 
 class GreedyBoss(unittest.TestCase):
 
-    def test_greedy_boss(self):
+    def test_greedy_boss1(self):
         computed = greedy_boss(35, 100)
-        expected = [(0, 0), (10, 1000), (16, 2200), (20, 3400), (23, 4600), (26, 6100), (29, 7900), (31, 9300), (33, 10900), (35, 12700)]
+        expected = [(0, 0), (10, 1000), (16, 2200), (20, 3400), (23, 4600), (26, 6100),
+                    (29, 7900), (31, 9300), (33, 10900), (35, 12700)]
+        self.assertEqual(computed, expected)
+
+    def test_greedy_boss2(self):
+        computed = greedy_boss(35, 0)
+        expected = [(0, 0), (10, 1000), (15, 2000), (19, 3200), (21, 4000),
+                    (23, 5000), (25, 6200), (27, 7600), (28, 8400), (29, 9300), (30, 10300),
+                    (31, 11400), (32, 12600), (33, 13900), (34, 15300), (34, 15300), (35, 16900)]
         self.assertEqual(computed, expected)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(GreedyBoss)
-# unittest.TextTestRunner(verbosity=0).run(suite)
+unittest.TextTestRunner(verbosity=2).run(suite)
