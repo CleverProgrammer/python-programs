f64f8e3f8b3b27d25da8da8d28f503a4e9b6aff4
rafeh01 <rafehqazi1@gmail.com>
2015-10-05 17:23:49 -0500

    adding string representation to my cookie_clicker.py game

--- coursera/poc1/week5/cookie_clicker.py
+++ coursera/poc1/week5/cookie_clicker.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2015-10-05 01:28:26
-# @Author  : Rafeh Qazi (rafehqazi1@gmail.com)
-# @Link    : 
-# @Version : $Id$
 """
+ -*- coding: utf-8 -*-
+@Date    : 2015-10-05 01:28:26
+@Author  : Rafeh Qazi (rafehqazi1@gmail.com)
+@Link    : http://www.codeskulptor.org/#user40_z5Pd9GiYA8_2.py
 Cookie Clicker Simulator
 """
 
@@ -19,29 +17,46 @@ import poc_clicker_provided as provided
 # Constants
 SIM_TIME = 10000000000.0
 
+# -----------------------------------ClickerState Class-------------------------------------------
+
+
 class ClickerState:
+
     """
     Simple class to keep track of the game state.
     """
-    
+
     def __init__(self):
+        self.total_cookies = 0.0
+        self.current_cookies = 0.0
+        self.current_seconds = 0.0
+        self.current_cps = 1.0
         pass
-        
+
     def __str__(self):
         """
         Return human readable state
         """
-        return "not yet implemented"
-        
-    def get_cookies(self):
+        items = {'total_cookies': self.total_cookies, 
+        'current_cookies': self.current_cookies,
+         'current_seconds': self.current_seconds,
+          'current_cps': self.current_cps}
+        string = ""
+        for item in items:
+            string += str(item) + "\n"
+
+    # def print_history(self):
+    #    pass
+
+    def get_total_cookies(self):
         """
-        Return current number of cookies 
-        (not total number of cookies)
-        
+        Return current number of total_cookies 
+        (not total number of total_cookies)
+
         Should return a float
         """
         return 0.0
-    
+
     def get_cps(self):
         """
         Get current CPS
@@ -49,7 +64,7 @@ class ClickerState:
         Should return a float
         """
         return 0.0
-    
+
     def get_time(self):
         """
         Get current time
@@ -57,13 +72,13 @@ class ClickerState:
         Should return a float
         """
         return 0.0
-    
+
     def get_history(self):
         """
         Return history list
 
         History list should be a list of tuples of the form:
-        (time, item, cost of item, total cookies)
+        (time, item, cost of item, total total_cookies)
 
         For example: [(0.0, None, 0.0, 0.0)]
 
@@ -72,15 +87,15 @@ class ClickerState:
         """
         return []
 
-    def time_until(self, cookies):
+    def time_until(self, total_cookies):
         """
-        Return time until you have the given number of cookies
-        (could be 0.0 if you already have enough cookies)
+        Return time until you have the given number of total_cookies
+        (could be 0.0 if you already have enough total_cookies)
 
         Should return a float with no fractional part
         """
         return 0.0
-    
+
     def wait(self, time):
         """
         Wait for given amount of time and update state
@@ -88,7 +103,7 @@ class ClickerState:
         Should do nothing if time <= 0.0
         """
         pass
-    
+
     def buy_item(self, item_name, cost, additional_cps):
         """
         Buy an item and update state
@@ -96,8 +111,10 @@ class ClickerState:
         Should do nothing if you cannot afford the item
         """
         pass
-   
-    
+
+# -----------------------------------Simulation Functions-------------------------------------------
+
+
 def simulate_clicker(build_info, duration, strategy):
     """
     Function to run a Cookie Clicker game for the given
@@ -109,7 +126,7 @@ def simulate_clicker(build_info, duration, strategy):
     return ClickerState()
 
 
-def strategy_cursor_broken(cookies, cps, history, time_left, build_info):
+def strategy_cursor_broken(total_cookies, cps, history, time_left, build_info):
     """
     Always pick Cursor!
 
@@ -122,7 +139,8 @@ def strategy_cursor_broken(cookies, cps, history, time_left, build_info):
     """
     return "Cursor"
 
-def strategy_none(cookies, cps, history, time_left, build_info):
+
+def strategy_none(total_cookies, cps, history, time_left, build_info):
     """
     Always return None
 
@@ -131,24 +149,28 @@ def strategy_none(cookies, cps, history, time_left, build_info):
     """
     return None
 
-def strategy_cheap(cookies, cps, history, time_left, build_info):
+
+def strategy_cheap(total_cookies, cps, history, time_left, build_info):
     """
     Always buy the cheapest item you can afford in the time left.
     """
     return None
 
-def strategy_expensive(cookies, cps, history, time_left, build_info):
+
+def strategy_expensive(total_cookies, cps, history, time_left, build_info):
     """
     Always buy the most expensive item you can afford in the time left.
     """
     return None
 
-def strategy_best(cookies, cps, history, time_left, build_info):
+
+def strategy_best(total_cookies, cps, history, time_left, build_info):
     """
     The best strategy that you are able to implement.
     """
     return None
-        
+
+
 def run_strategy(strategy_name, time, strategy):
     """
     Run a simulation for the given time with one strategy.
@@ -156,19 +178,20 @@ def run_strategy(strategy_name, time, strategy):
     state = simulate_clicker(provided.BuildInfo(), time, strategy)
     print strategy_name, ":", state
 
-    # Plot total cookies over time
+    # Plot total total_cookies over time
 
-    # Uncomment out the lines below to see a plot of total cookies vs. time
+    # Uncomment out the lines below to see a plot of total total_cookies vs. time
     # Be sure to allow popups, if you do want to see it
 
     # history = state.get_history()
     # history = [(item[0], item[3]) for item in history]
-    # simpleplot.plot_lines(strategy_name, 1000, 400, 'Time', 'Total Cookies', [history], True)
+    # simpleplot.plot_lines(strategy_name, 1000, 400, 'Time', 'Total total_cookies', [history], True)
+
 
 def run():
     """
     Run the simulator.
-    """    
+    """
     run_strategy("Cursor", SIM_TIME, strategy_cursor_broken)
 
     # Add calls to run_strategy to run additional strategies
@@ -176,16 +199,47 @@ def run():
     # run_strategy("Expensive", SIM_TIME, strategy_expensive)
     # run_strategy("Best", SIM_TIME, strategy_best)
 
-    
-test_obj = ClickerState()
+
+# -----------------------------------PLAYGROUND-------------------------------------------
 run()
 
 obj = provided.BuildInfo()
 obj.clone()
-print("build_items: %s" %str(obj.build_items()))
-print("Grandma COST: %s" %obj.get_cost('Grandma'))
-print("Grandma CPS: %s" %obj.get_cps('Grandma'))
+print("build_items: %s" % str(obj.build_items()))
+print("Grandma COST: %s" % obj.get_cost('Grandma'))
+print("Grandma CPS: %s" % obj.get_cps('Grandma'))
 obj.update_item('Grandma')
-print("Grandma COST: %s" %obj.get_cost('Grandma'))
-    
+print("Grandma COST: %s" % obj.get_cost('Grandma'))
 
+
+# -----------------------------------GREEDYBOSS-------------------------------------------
+# Greedy boss function. Need to use later to help in cookie_clicker
+def greedy_boss(days_in_simulation, bribe_cost_increment):
+    """
+    Simulation of greedy boss
+    """
+
+    # initialize necessary local variables
+    current_day = 0
+    current_salary = INITIAL_SALARY
+    total_salary = 0
+    current_bribe_cost = INITIAL_BRIBE_COST
+    current_money = 0
+    days_till_next_bribe = 0
+    # initialize list consisting of days vs. total salary earned for analysis
+    days_vs_earnings = [(0, 0)]
+
+    # Each iteration of this while loop simulates one bribe
+    while current_day <= days_in_simulation:
+        days_till_next_bribe = math.ceil((current_bribe_cost - current_money) / current_salary)
+        current_day += days_till_next_bribe
+        current_money += current_salary * days_till_next_bribe
+        total_salary += current_salary * days_till_next_bribe
+        # check whether we have enough savings to bribe without waiting
+        while current_money >= current_bribe_cost:
+            if current_day > days_in_simulation:
+                return days_vs_earnings
+            current_money -= current_bribe_cost
+            days_vs_earnings.append((current_day, total_salary))
+            current_salary += SALARY_INCREMENT
+            current_bribe_cost += bribe_cost_increment
