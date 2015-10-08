e92c5bea445fb91ff97b6813ef52bcd542c060d4
rafeh01 <rafehqazi1@gmail.com>
2015-10-07 20:49:04 -0500

    adding some new files

---
+++ project_euler/largest_prime_factor.py
@@ -0,0 +1,24 @@
+#import cProfile 
+
+def is_prime(number):
+ for i in range(2, number//4+1):
+  if number%i == 0:
+   return False
+ return True
+
+def largest_prime_factor(number):
+ if is_prime(number//2):
+  return number//2
+ else:
+  for i in range(2, number//4+1)[::-1]:
+   if number % i == 0:
+    if is_prime(i):
+     return i
+ return None
+
+
+#(is_prime(786))
+#largest_prime_factor(2477238317)
+#print(largest_prime_factor(200000))
+#print(largest_prime_factor(600851475143))
+
