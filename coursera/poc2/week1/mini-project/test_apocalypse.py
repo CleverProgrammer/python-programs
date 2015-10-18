41f693f1f534b59b5b03ff0ec39f8529c7835c6c
rafeh01 <rafehqazi1@gmail.com>
2015-10-17 22:43:07 -0500

    found an amazing screen layout

--- coursera/poc2/week1/mini-project/test_apocalypse.py
+++ coursera/poc2/week1/mini-project/test_apocalypse.py
@@ -8,4 +8,14 @@ class TestApocalypse(TestCase):
   def test_num_humans(self):
     state = Apocalypse(5, 5)
     state.add_human(2, 3)
-    self.assertEqual(state.num_humans(), 1, "test 1")
+    self.assertEqual(state.num_humans(), 1, "test num humans")
+
+  def test_clear(self):
+      state = Apocalypse(5, 5)
+      state.add_human(2, 3)
+      state.add_human(1, 2)
+      self.assertEqual(state.num_humans(), 2)
+      state.clear()
+      self.assertEqual(state.num_humans(), 0)
+
+
