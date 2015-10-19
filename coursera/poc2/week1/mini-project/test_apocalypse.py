18327aba999f9eed6377e856cbecab5cf2476d35
rafeh01 <rafehqazi1@gmail.com>
2015-10-18 21:15:49 -0500

    comitting all progress on my iMac to pick it up on my laptop

--- coursera/poc2/week1/mini-project/test_apocalypse.py
+++ coursera/poc2/week1/mini-project/test_apocalypse.py
@@ -86,12 +86,10 @@ class TestApocalypse(TestCase):
         human_generator = state.humans()
         human = next(human_generator)
         self.assertEqual(human, (2,2), "test human generator")
-        neighbors = state.four_neighbors(human[0], human[1])
-        self.assertEqual(neighbors, [(1, 2), (3, 2), (2, 1), (2, 3)], "testing human's neighbors")
+        neighbors = state.eight_neighbors(human[0], human[1])
+        self.assertEqual(neighbors, [(1, 2), (3, 2), (2, 1), (2, 3), (1, 1), (1, 3), (3, 1), (3, 3)],
+                         "testing human's neighbors")
         human_distance = zombie_distance_field[human[0]][human[1]]
-        print(neighbors[0][0])
-        print(zombie_distance_field[neighbors[0][0]][neighbors[0][1]])
-        print(human_distance)
         safest_distance = 10
         safest_location = 0
         for neighbor in neighbors:
@@ -100,4 +98,4 @@ class TestApocalypse(TestCase):
             if zombie_distance_field[neighbor[0]][neighbor[1]] < safest_distance:
                 safest_distance = zombie_distance_field[neighbor[0]][neighbor[1]]
                 safest_location = neighbor
-        print("neighbor:", neighbor)
+        print("safest_location:", safest_location)
